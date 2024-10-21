1a47ca50be02d849d6f6f8222da6642fc6200497
Amy Storm Kosman <29407286+alstormyk@users.noreply.github.com>
2024-10-21 17:28:24 +0200

    Simplifiy: Update build.sh

--- build.sh
+++ build.sh
@@ -6,15 +6,16 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-curl -Lo /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+# curl -Lo /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+# rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen vlc brave-browser
+rpm-ostree install screen vlc 
+# brave-browser
 
 #### Example for enabling a System Unit File
 
