f31f214618f6c4f01381cd97fd72ee1d678d601b
Amy Storm Kosman <29407286+alstormyk@users.noreply.github.com>
2024-10-21 20:51:00 +0200

    NEW: Qtile-wayland Update build.sh

--- build.sh
+++ build.sh
@@ -6,21 +6,17 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-curl -Lo /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install -y screen qtile-wayland vlc
+rpm-ostree install qtile-wayland
 
-# remove packages
-rpm-ostree override remove gnome*
-
-# clean up packages
-rpm-ostree cleanup -m
+# this would install a package from rpmfusion
+# rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
