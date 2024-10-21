ef5219f4e54187aa8dcdc66b67a994259892b153
Amy Storm Kosman <29407286+alstormyk@users.noreply.github.com>
2024-10-21 16:27:16 +0200

    Try Again: Consolidate install Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ RELEASE="$(rpm -E %fedora)"
 ### Install packages
 curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo | tee /etc/yum.repos.d/brave-browser.repo
 curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-core.asc | tee /etc/pki/rpm-gpg/brave-core.asc
-rpm-ostree install brave-browser
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
@@ -16,10 +15,7 @@ rpm-ostree install brave-browser
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-rpm-ostree install vlc
+rpm-ostree install screen vlc brave-browser
 
 #### Example for enabling a System Unit File
 
