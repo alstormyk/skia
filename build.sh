2943f0f1bfa712ffb68ff72ce6c02db5fdb2d4b3
Amy Storm Kosman <29407286+alstormyk@users.noreply.github.com>
2024-10-21 16:12:39 +0200

    Try Again: Add brave-browser Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-
+curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo | sudo tee /etc/yum.repos.d/brave-browser.repo
+curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-core.asc | sudo tee /etc/pki/rpm-gpg/brave-core.asc
+rpm-ostree install brave-browser
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
