d5848766c9b45ac7dc4917dbdd53e4b0fc2272a7
Amy Storm Kosman <29407286+alstormyk@users.noreply.github.com>
2024-10-21 16:19:54 +0200

    Try Again: Install brave-browser Update build.sh
    I don't think I need sudo. Take it out.

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
-curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo | sudo tee /etc/yum.repos.d/brave-browser.repo
-curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-core.asc | sudo tee /etc/pki/rpm-gpg/brave-core.asc
+curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo | tee /etc/yum.repos.d/brave-browser.repo
+curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-core.asc | tee /etc/pki/rpm-gpg/brave-core.asc
 rpm-ostree install brave-browser
 
 # Packages can be installed from any enabled yum repo on the image.
