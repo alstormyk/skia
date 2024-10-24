1cce34ef206f7bf0a2ace95aa6fa8a52d76377a0
Amy Storm Kosman <29407286+alstormyk@users.noreply.github.com>
2024-10-24 11:08:56 +0200

    Fix: variable

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
-FEDORA_VERSION=40
+FEDORA_VERSION='40'
 
 
 ### Install packages
@@ -14,7 +14,7 @@ FEDORA_VERSION=40
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-curl -Lo "https://copr.fedorainfracloud.org/coprs/yalter/niri/repo/fedora-"${FEDORA_VERSION}"/yalter-niri-fedora-"${FEDORA_VERSION}".repo" > /etc/yum.repos.d/yalter-niri-fedora-"${FEDORA_VERSION}".repo
+curl -Lo "https://copr.fedorainfracloud.org/coprs/yalter/niri/repo/fedora-"$FEDORA_VERSION"/yalter-niri-fedora-'$FEDORA_VERSION'.repo" > /etc/yum.repos.d/yalter-niri-fedora-'$FEDORA_VERSION'.repo
 rpm-ostree install niri
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
