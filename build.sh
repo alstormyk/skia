9e0bdce6f2ef941c110f9ca9eafd54db0e5ffe6c
Amy Storm Kosman <29407286+alstormyk@users.noreply.github.com>
2024-10-24 11:27:13 +0200

    Try: variable Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-curl "https://copr.fedorainfracloud.org/coprs/yalter/niri/repo/fedora-40/yalter-niri-fedora-40.repo" > /etc/yum.repos.d/yalter-niri-fedora-40.repo
+curl "https://copr.fedorainfracloud.org/coprs/yalter/niri/repo/fedora-$RELEASE/yalter-niri-fedora-40.repo" > /etc/yum.repos.d/yalter-niri-fedora-40.repo
 rpm-ostree install niri
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
